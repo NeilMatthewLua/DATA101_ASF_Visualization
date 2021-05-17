bb103b666fa59f979ceb65cce219ff94c96a8901
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2021-05-17 09:50:25 +0800

    Added theme provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.lightBlue,
+    height: '100vh',
+    width: '100vw',
+    paddingTop: '10px'
+  }, 
+  titleText: {
+    color: theme.palette.white,
+    textAlign: 'center'
+  }
+}));
 
 function App() {
+  const theme =  useTheme(); 
+  const classes = useStyle(theme); 
+
   return (
-    <div className="App">
+    <div className={classes.root}>
+      <h1 className={classes.titleText}>
+        Interactive ASF Tracker
+      </h1>
     </div>
   );
 }
