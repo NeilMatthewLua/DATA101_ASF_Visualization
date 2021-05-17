efd992524e9f5c6d61036b6cc6ae2ffee28c6d77
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2021-05-17 11:31:19 +0800

    added sample data for App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useTheme, makeStyles } from '@material-ui/core/styles';
+import SidebarChart from './components/SidebarChart';
 
 const useStyle = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.lightBlue,
     height: '100vh',
     width: '100vw',
+    maxWidth: '100%',
+    maxHeight: '100%',
     paddingTop: '10px'
   }, 
   titleText: {
@@ -16,15 +19,23 @@ const useStyle = makeStyles(theme => ({
   }
 }));
 
+const data = [
+  {year: 2017, sales: 360000},
+  {year: 2018, sales: 687300},
+  {year: 2020, sales: 608100},
+]
+
 function App() {
   const theme =  useTheme(); 
-  const classes = useStyle(theme); 
+  const classes = useStyle(theme);
+  const [chartData, setChartData] = useState(data);
 
   return (
     <div className={classes.root}>
       <h1 className={classes.titleText}>
         Interactive ASF Tracker
       </h1>
+      <SidebarChart regionName={"Region XII"} data={chartData}/>
     </div>
   );
 }
