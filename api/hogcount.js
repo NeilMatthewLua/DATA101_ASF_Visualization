e6723bb8a30894444daa1a5dae950aa8175043af
Kurotwo <kurt_bradley_tanting@dlsu.edu.ph>
2021-05-20 14:53:14 +0800

    Test adding hogcount file in api

---
+++ api/hogcount.js
@@ -0,0 +1,11 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const rawHogCount = readFileSync(join(__dirname, 'data', 'hog_count.json'), 'utf8');
+
+module.exports = (req, res) => {
+    let regions = req.body.regions;
+    let hogCount = JSON.parse(rawHogCount);
+    let filteredResults = hogCount.filter((region) => regions.includes(region.region));
+    console.log(hogCount);
+    res.send(filteredResults);
+}
