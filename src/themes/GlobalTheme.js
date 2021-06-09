269b8629aeae4802332f77c71ed246e96a205e72
neil matthew lua <neil_lua@dlsu.edu.ph>
2021-06-09 09:45:35 +0800

    Revert commit

---
+++ src/themes/GlobalTheme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        darkBlue:           '#487B94',
+        lightBlue:          '#98BBD6',
+        darkGreen:          '#218173',
+        lightGreen:         '#7EC7AE',
+        neutralGreen:       '#D3DEDC',
+        white:              '#FFFFFF',
+        black:              '#000000',
+        qualitativeLight:   '#FDBE85',
+        qualitativeNeutral: '#FD8D3C',
+        qualitativeDark:    '#D94701',
+        errorRed:           '#D34956',
+    }
+});
+
+export default theme;
